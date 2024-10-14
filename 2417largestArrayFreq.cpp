b90cfaadb3d930720921aa1cd9ffba86489bc66b
siqi wei <weisiqi19@sjtu.edu.cn>
2024-10-14 11:05:59 +0800

    Create 2417largestArrayFreq.cpp

---
+++ 2417largestArrayFreq.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm> // 可能用于后续扩展
+
+int main() {
+    int n; // array length
+    std::cin >> n; 
+
+    std::vector<int> a(n); 
+
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i]; 
+    }
+
+    // store frequency with unordered map
+    std::unordered_map<int, int> frequency;
+
+	// calculate each number's frequency
+    for (int num : a) {
+        ++frequency[num];
+    }
+
+	// find the max frequency
+    int maxFrequency = 0;
+    for (const auto& pair : frequency) {
+        if (pair.second > maxFrequency) {
+            maxFrequency = pair.second;
+        }
+    }
+
+    std::cout << maxFrequency;
+
+    return 0;
+}
